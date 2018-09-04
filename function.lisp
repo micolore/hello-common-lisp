cd9979e6cd34c39c09c937755cd31198483d5347
bhw <471304402@qq.com>
2018-09-04 01:06:02 -0400

    function 2

--- function.lisp
+++ function.lisp
@@ -58,6 +58,8 @@
 (foo  :a 1 :b 2 :c 3) ;;(1 2 3)
 (foo  :a 1 :c 3 :b 2);;(1 2 3)
 
+;; 如同可选形参那样，关键字形参也提供一个默认值形式以及supplied-p
+
 (defun  foo (&key (a 0) (b 0 b-supplied-p) (c (+ a b)))
   (list a  b c b-supplied-p))
 (foo :a 1) ;; (1 0 1 NIL)
@@ -65,6 +67,29 @@
 (foo :b 1 :c 4) ;;(0 1 4 T)
 (foo :a 2 :b 1 :c 4 ;;(2 1 4 T)   
 
+(defun foo(&key ((:apple a)) ((:box b)) ((:charlie c) 0 c-supplied-p))
+  (list :apple 10 :box 20 :charlie  30) ;; (10  20 30 T)
+  
+;; 参数顺序 必要形参、可选形参、剩余形参、关键字形参
+;; 尽量避免 &optional 与&key一起使用
 
 
+  (defun  foo  (x &optional y &key z) (list x y z))
+  (foo 1 2 :z 3) ;;(1 2 3)
+  (foo 1 ) ;;(1 nil nil)
+  (foo 1 :z 3) ;; error
 
+
+  ;; 作为数据的高阶函数
+  (defun foo(x) (* x 2))
+  (function foo)
+  ;;通过函数对象调用函数
+  ;; FUNCALL APPLY
+
+  (foo 1 2 3) = (funcall #'foo 1 2 3)
+
+  (defun plot (fn min max step)
+    (loop for i from min to max  by step do
+         (loop repeat (funall fn i) do (format t "*"))
+         (format t "~")))
+  ;; apply 第二个参数可以是一个列表或者单独的实参
